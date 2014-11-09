b19a802cd442a08397dff4b37d8fb4e07bade37e
KevZho <KevZho141@gmail.com>
2014-11-08 16:09:54 -0800

    push sequelize objects

---
+++ framework/objects.js
@@ -0,0 +1,36 @@
+var Sequelize = require('sequelize'), sequelize = new Sequelize('testing', 'angelhack', 'angelhack', {
+	host: "angelhack.c626h2danuwm.us-west-2.rds.amazonaws.com"
+});
+// Objects to use with Sequelize
+var Test = sequelize.define('Test', {
+	name: Sequelize.STRING,
+	points: Sequelize.INTEGER,
+	created: Sequelize.DATE,
+	testDate: Sequelize.DATE,
+	available: Sequelize.BOOLEAN,
+	randomized: Sequelize.BOOLEAN
+});
+var Question = sequelize.define('Question', {
+	type: Sequelize.STRING,	
+	content: Sequelize.TEXT,
+	answers: Sequelize.TEXT,
+	correct: Sequelize.TEXT,
+	correctAnswerPoints: Sequelize.INTEGER,
+	noAnswerPoints: Sequelize.INTEGER,
+	wrongAnswerPoints: Sequelize.INTEGER,
+	random: Sequelize.BOOLEAN
+});
+Test.hasMany(Question);
+
+var User = sequelize.define("User", {
+	username: Sequelize.STRING,
+	password: Sequelize.STRING,
+	classes: Sequelize.ARRAY(Sequelize.INTEGER),
+	scores: Sequelize.TEXT
+});
+var Classes = sequelize.define("Class", {
+	name: Sequelize.TEXT
+});
+Classes.hasOne(User, {as: "admin"});
+Classes.hasMany(User, {as: "student"});
+Classes.hasMany(Test);
