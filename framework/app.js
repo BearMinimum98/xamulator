7c186ac2d2f82f6304026c7b8f0729807b899602
KevZho <KevZho141@gmail.com>
2014-11-08 16:32:14 -0800

    asdf

--- framework/app.js
+++ framework/app.js
@@ -32,16 +32,51 @@ app.use(bodyParser.urlencoded({
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-var connection = mysql.createConnection({
-	host: "angelhack.c626h2danuwm.us-west-2.rds.amazonaws.com",
-	user: "angelhack",
-	password: "angelhack",
-	database: "testing"
+/**
+ * Start sequelize
+ */
+var Sequelize = require('sequelize'), sequelize = new Sequelize('xamulatordb', 'xamulatoradmin', 'xamulatorswag', {
+	host: "xamulatordb.c626h2danuwm.us-west-2.rds.amazonaws.com",
+	port: 5432,
+	dialect: "postgres"
 });
+// Objects to use with Sequelize
+var Test = sequelize.define('Test', {
+	name: Sequelize.STRING,
+	points: Sequelize.INTEGER,
+	created: Sequelize.DATE,
+	testDate: Sequelize.DATE,
+	available: Sequelize.BOOLEAN,
+	randomized: Sequelize.BOOLEAN
+});
+var Question = sequelize.define('Question', {
+	type: Sequelize.STRING,	
+	content: Sequelize.TEXT,
+	answers: Sequelize.TEXT,
+	correct: Sequelize.TEXT,
+	correctAnswerPoints: Sequelize.INTEGER,
+	noAnswerPoints: Sequelize.INTEGER,
+	wrongAnswerPoints: Sequelize.INTEGER,
+	random: Sequelize.BOOLEAN
+});
+Test.hasMany(Question);
 
+var User = sequelize.define("User", {
+	username: Sequelize.STRING,
+	password: Sequelize.STRING,
+	classes: Sequelize.ARRAY(Sequelize.INTEGER),
+	scores: Sequelize.TEXT
+});
+var Classes = sequelize.define("Class", {
+	name: Sequelize.TEXT
+});
+Classes.hasOne(User, {as: "admin"});
+Classes.hasMany(User, {as: "student"});
+Classes.hasMany(Test);
+// END SEQUELIZE
 
-/** all functions and code definitions go below
+/** 
+ * all functions and code definitions go below
  */
 
 var dateNow = new Date().toISOString().slice(0, 19).replace("T", " ");
